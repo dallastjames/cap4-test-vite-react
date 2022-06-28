eb9c4609c24869f88a75a0a26d01050ccbebedc2
Dallas James <dallastjames@gmail.com>
2022-06-28 15:14:30 -0600

    init to win it

---
+++ android/app/src/main/java/com/dallastjames/cap4testvitereact/MainActivity.java
@@ -0,0 +1,5 @@
+package com.dallastjames.cap4testvitereact;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dallastjames.cap4testvitereact',
+  appName: 'cap4-test-vite-react',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
